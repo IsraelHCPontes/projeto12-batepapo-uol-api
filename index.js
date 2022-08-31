9b58e3657f27b903ed9164766ab3884f2577b0eb
israelHCPontes <israelhcpontes@gmail.com>
2022-08-31 01:09:37 -0300

    feat(pakcag) Configurei paacotes iniciais da API

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.listen(5000, () => console.log('Escutando na porta 5000'))
