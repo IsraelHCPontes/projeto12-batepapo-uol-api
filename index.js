ecbc79ed177abeddede87aa13058f294be1f2241
israelHCPontes <israelhcpontes@gmail.com>
2022-09-04 17:53:03 -0300

    feat(POST /messages)Implementei a postagem de messagens como todos os resquisitos)

--- index.js
+++ index.js
@@ -21,9 +21,16 @@ mongoClient.connect().then(() => {
 })
 
 const userSchema = joi.object({
-    name: joi.string().empty().required()
+    name: joi.string().required()
 })
 
+const msgSchema = joi.object({
+    
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message','private_message').required(),
+})
+ 
 
 
 server.post("/participants", async (req, res) => {
@@ -37,8 +44,8 @@ server.post("/participants", async (req, res) => {
     }
     
     try{
-
-        const userExiste = await db.collection("participants").findOne({name:name});
+                         
+        const userExiste = await db.collection("participants").findOne({name});
         
         if(userExiste){
             res.status(409).send({message:"Usuario ja existe"})
@@ -57,7 +64,7 @@ server.post("/participants", async (req, res) => {
 
         res.sendStatus(201)
     }catch{
-        res.status(422)
+        res.sendStatus(422)
         
     }   
 })
@@ -68,10 +75,38 @@ server.get('/participants', async (req, res) =>{
     const participantes = await db.collection('participants').find().toArray()
     res.status(200).send(participantes)
     }catch(err){
-        res.send(err)
+        res.status(500).send(err.message)
+    }
+})
+
+server.post('/messages', async (req, res ) => {
+    const {to, text, type} = req.body
+    const  user = req.headers.user
+    const time = dayjs().format('HH:mm:ss');
+    const validation = msgSchema.validate({to, text, type})
+
+    if(validation.error){
+        res.status(422).send('Erro de menssagem')
     }
 
-    
+    try{
+       const userExiste = await db.collection("participants").findOne({name:user})
+
+       if(userExiste){
+        await db.collection('messages').insertOne({
+            from:user,
+            to,
+            text,
+            type,
+            time,
+       })
+       res.sendStatus(201)
+       }else{
+        res.status(422).send('Usuario não cadastrado')
+       }
+    }catch(err){
+        res.sendStatus(500)
+    }
 })
 
 server.listen(5000, () => console.log('Escutando na porta 5000'))
